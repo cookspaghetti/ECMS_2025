288917e18400dcfd21e2a85153713d9c4e04a922
cookspaghetti <138357780+cookspaghetti@users.noreply.github.com>
2025-07-16 22:28:56 +0800

    fix: removed unused dto cpp files

--- src/dto/Match.cpp
+++
@@ -1,6 +0,0 @@
-#include "Match.hpp"
-
-Match::Match() : id(0), tournamentId(0), matchType(MatchType::BestOf1), date(""), time(""), player1(0), player2(0), resultId(0) {}
-
-Match::Match(int id, int tournamentId, MatchType matchType, const std::string& date, const std::string& time, int player1, int player2, int resultId)
-    : id(id), tournamentId(tournamentId), matchType(matchType), date(date), time(time), player1(player1), player2(player2), resultId(resultId) {}
--- src/dto/Performance.cpp
+++
@@ -1,8 +0,0 @@
-#include "Performance.hpp"
-
-Performance::Performance() : playerId(0), matchesPlayed(0), favouriteChampion(Champion::Ahri), lastWin(""), winRate(0.0f) {}
-
-Performance::Performance(int playerId, int matchesPlayed, Champion favouriteChampion,
-                         const std::string& lastWin, float winRate)
-    : playerId(playerId), matchesPlayed(matchesPlayed), favouriteChampion(favouriteChampion),
-      lastWin(lastWin), winRate(winRate) {}
